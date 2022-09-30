3cb096db9bc7ef6ac882ec4b240a497451a79f7c
Aravind Krishna <akaravindk59@gmail.com>
2022-09-30 22:13:35 +0530

    make it better

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ app.use('/api/users', usersRoute)
 app.use('/api/hotels', hotelsRoute)
 app.use('/api/rooms', roomsRoute)
 
-app.use('/', (req, res) => {
-  res.send('Welcome to StayQuick API')
-})
+// app.use('/', (req, res) => {
+//   res.send('Welcome to StayQuick API')
+// })
 
 app.use((err, req, res, next) => {
   const errorStatus = err.status || 500
